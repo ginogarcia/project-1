a0dd3ff62a0708c28c0c07f8d320ee578fc2a0ef
Gino Garcia <ginogarcia@netzero.net>
2012-02-23 00:12:47 -0600

    controller pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+  def new
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Project1::Application.routes.draw do
+  get "pages/new"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
